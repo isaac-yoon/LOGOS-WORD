3f08c277ad9a3f84fd8ade021569da8a683aa36d
Isaac Yoon <60016341+isaac-yoon@users.noreply.github.com>
2025-06-20 09:20:15 -0700

    Update verses.js
    Signed-off-by: Isaac Yoon <60016341+isaac-yoon@users.noreply.github.com>

--- public/javascripts/verses.js
+++ public/javascripts/verses.js
@@ -1,7 +1,9 @@
 const verses = {
-    '',
-    '',
-    '',
+    'I am Alpha and Omega, the beginning and the ending, saith the Lord, which is, and which was, and which is to come, the Almighty',
+    'I AM THAT I AM . . . Thus shalt thou say unto the children of Israel, I AM hath sent me unto you.',
+    'For the life was manifested, and we have seen it, and bear witness, and shew unto you that eternal life, which was with the Father, and was manifested unto us',
+    'Thou art worthy, O Lord, to receive glory and honour and power: for thou hast created all things, and for thy pleasure they are and were created.',
+    'The grace of our Lord Jesus Christ be with you all. Amen.',
 }
 
 export default verses;
